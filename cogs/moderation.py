fa0596eafd6d9002d0db2e7a6be6b7623cc7ac9d
Pixelio <drl@net-change.com>
2017-10-23 22:37:55 -0400

    Few fixes, location changes

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -2,6 +2,8 @@ from discord.ext import commands
 import discord
 import logging
 import asyncio
+import requests
+import time
 from .utils import checks
 from .utils.embed import Embeds
 
@@ -23,11 +25,44 @@ class Moderation:
                 messages = []
             await asyncio.sleep(1.5)
 
-    @commands.group(pass_context=True, no_pm=True, aliases=['purge', 'clear'])
+    @commands.command(pass_context=True, no_pm=True, aliases=['botinfo', 'stats', 'about'])
+    async def info(self, ctx): # !!info
+        """Shows bot info."""
+        try:
+            await self.bot.send_typing(ctx.message.channel)
+            pyver = ""
+            for x in sys.version_info[0:3]:
+                if x == sys.version_info[2]:
+                    pyver += str(x)
+                else:
+                    pyver += str(x) + "."
+            elapsed_time = time.gmtime(time.time() - self.bot.start_time)
+            log.info("Got bot uptime")
+            stp = str(elapsed_time[7] - 1) + " days, " + str(elapsed_time[3]) + " hours, " + str(elapsed_time[4]) + " minutes"
+            log.info("Formatted bot uptime")
+            users = sum(1 for _ in self.bot.get_all_members())
+            log.info("Got all bot users")
+            color = discord.Color.default()
+            if ctx.message.server is not None:
+                color = ctx.message.server.me.color
+            embed = discord.Embed(color=color, title="NanoBot Statistics", description="Made by **Der#4587** and **Kalico#5669**")
+            embed.set_footer(text="NanoBot#2520")
+            embed.set_thumbnail(url=self.bot.user.avatar_url)
+            embed.add_field(name="> Uptime", value=stp)
+            embed.add_field(name="> Usage", value="**• Guilds:** {}\n**• Users:** {}".format(len(self.bot.servers), users))
+            embed.add_field(name="> Commands", value="**• Total Received:** {}\n**• Errors:** {} ({}%)".format(len(self.bot.cmds_this_session), len(self.bot.errors), round(len(self.bot.errors)/len(self.bot.cmds_this_session) * 100)))
+            embed.add_field(name="> Voice", value="**• Active Sessions:** {}\n**• Songs Played:** Unknown".format(len(self.bot.voice_clients)))
+            embed.add_field(name="> Version", value="**• NanoBot:** 2.0\n**• discord.py:** {}\n**• Python:** {}".format(discord.__version__, pyver))
+            embed.add_field(name="> Misc", value="**• Website:** [Go!](https://nanobot-discord.github.io)\n**• Discord:** [Join!](https://discord.gg/eDRnXd6)")
+            log.info("Created Embed")
+            await self.bot.say(embed=embed)
+        except:
+            raise       
+            
+    @commands.command(pass_context=True, no_pm=True, aliases=['purge', 'clear'])
     @checks.mod_or_permissions(manage_messages=True)
     async def prune(self, ctx, number: int):
         """Deletes the specified amount of messages."""
-
         if ctx.invoked_subcommand is None:
             channel = ctx.message.channel
             author = ctx.message.author
@@ -46,7 +81,7 @@ class Moderation:
 
             log.info("{}({}) deleted {} messages in channel {}".format(author.name, author.id, number, channel.name))
             await self.process_deletion(to_delete)
-
+        
 def setup(bot):
     n = Moderation(bot)
     bot.add_cog(n)
